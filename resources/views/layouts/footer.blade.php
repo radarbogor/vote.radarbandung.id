dbdcd8223d8cd78acbff7b657df0dae68e3ad09b
radarbogor <radarbogor.dev@gmail.com>
2023-10-04 10:57:38 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,73 @@
+
+{{-- Footer Layout --}}
+<footer class="text-light py-3" style="background-color: #00548F;">
+  <div class="container text-md-left">
+    <div class="row text-md-left pt-3">
+      <div class="col-md-6">
+        <img src="{{ asset('img/navlogo.png') }}" alt="logo radar bandung" class="mb-0" style="width: 250px">
+        <hr>
+        <div class="d-flex">
+          <i class="fas fa-map-marker-alt mt-1"></i>
+          <p class="ms-3">Jl. H. Ibrahim Adjie (Kiaracondong) No 95 Bandung</p>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <h5 class="text-uppercase mb-4 fw-bold">Social</h5>
+        <hr>
+        <p>
+          <a class="text-decoration-none text-light" href="https://www.youtube.com/@radarbandungnews" target="_blank">
+            <i class="fab fa-youtube me-3"></i>Youtube
+          </a>
+        </p>
+        <p>
+          <a class="text-decoration-none text-light" href="https://www.tiktok.com/@radarbandung" target="_blank">
+            <i class="fab fa-tiktok me-3"></i>Tiktok
+          </a>
+        </p>
+        <p>
+          <a class="text-decoration-none text-light" href="https://www.instagram.com/radarbandung.id/" target="_blank">
+            <i class="fab fa-instagram me-3"></i>Instagram
+          </a>
+        </p>
+        <p>
+          <a class="text-decoration-none text-light" href="https://www.facebook.com/RadarBandung/" target="_blank">
+            <i class="fab fa-facebook me-3"></i>Facebook
+          </a>
+        </p>
+        <p>
+          <a class="text-decoration-none text-light" href="https://twitter.com/radarbandung" target="_blank">
+            <i class="fab fa-twitter me-3"></i>Twitter
+          </a>
+        </p>
+        <p>
+          <a class="text-decoration-none text-light" href="https://www.snackvideo.com/@radarbandung.id" target="_blank">
+            <i class="fa-solid fa-play me-3"></i>Snack Video
+          </a>
+        </p>
+      </div>
+
+      <div class="col-md-3">
+        <h5 class="text-uppercase mb-4 fw-bold">Contact</h5>
+        <hr>
+        <p>
+          <i class="fas fa-envelope me-3"></i>official@radarbandung.id
+        </p>
+        <p>
+          <i class="fas fa-phone me-3"></i>022-7233819
+        </p>
+        <p>
+          <i class="fas fa-phone me-3"></i>022-7238058
+        </p>
+      </div>
+      <hr>
+      <div class="d-flex align-items-center">
+          <p class="mb-0">&copy {{date('Y')}}
+            <a href="#" style="text-decoration: none;">
+              <strong class="text-light">Radar Bandung</strong>
+            </a>
+          </p>
+      </div>
+    </div>
+  </div>
+</footer>
